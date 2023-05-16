7f0ce99542d13e5aae56465dedc948f455ad30c9
aliaksandra.arkhipenka <aliaksandra.arkhipenka@godeltech.com>
2023-05-16 13:37:01 +0200

    Folder changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
-import Footer from "./components/Layout/Footer";
-import Header from "./components/Layout/Header";
+import Footer from "./components/Layout/Footer/Footer";
+import Header from "./components/Layout/Header/Header";
 
 function App() {
   return (
