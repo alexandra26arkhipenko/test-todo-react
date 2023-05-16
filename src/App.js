a5d3718e49883eab9ad4c2598c9f20b29b099390
GODELTECH\aliaksandra.arkhipen <aliaksandra.arkhipenka@godeltech.com>
2023-05-16 13:12:04 +0200

    folders are added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
-import Header from "./components/Layout/Header";
-import Footer from "./components/Layout/Footer";
+import Header from "./components/Layout/header/Header";
+import Footer from "./components/Layout/footer/Footer";
 
 function App() {
   return (
