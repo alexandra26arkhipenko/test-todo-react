99b8c8e9956a3780c077d46d01de99e341efedfc
aliaksandra.arkhipenka <aliaksandra.arkhipenka@godeltech.com>
2023-05-17 16:56:21 +0200

    fix

--- src/store/todo.js
+++ src/store/todo.js
@@ -1,14 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { uuid } from "uuidv4";
+import { v4 as uuid } from "uuid";
 
 const initialState = {
   todos: [
-    { id: 123, text: "kill yourself", isComplited: false },
-    { id: 456, text: "you really should to it", isComplited: true },
-    { id: 789, text: "find new job", isComplited: true },
-    { id: 101112, text: "eat lunch", isComplited: false },
-    { id: 131415, text: "get platinim in the witcher", isComplited: false },
-    { id: 161718, text: "go to Paris", isComplited: true },
+    { id: uuid(), text: "kill yourself", isComplited: false },
+    { id: uuid(), text: "you really should to it", isComplited: true },
+    { id: uuid(), text: "find new job", isComplited: true },
+    { id: uuid(), text: "eat lunch", isComplited: false },
+    { id: uuid(), text: "get platinim in the witcher", isComplited: false },
+    { id: uuid(), text: "go to Paris", isComplited: true },
   ],
 };
 
