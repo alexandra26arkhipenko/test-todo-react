4a4c791e0305082e046b50cac21e76d509f8d9c3
aliaksandra.arkhipenka <aliaksandra.arkhipenka@godeltech.com>
2023-05-21 12:47:25 +0200

    Todo tests

--- src/components/Todos/Todo/Todo.test.js
+++ src/components/Todos/Todo/Todo.test.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import Todo from "./Todo";
 import { Provider } from "react-redux";
-import store from "../../../store/store";
+import configureMockStore from "redux-mock-store";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { todoActions } from "../../../store/todo";
 
 const filteredTodos = [{ id: "1", text: "text", isCompleted: false }];
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe("Todo", () => {
   it("should render correctly", () => {
@@ -32,5 +35,63 @@ describe("Todo", () => {
       </Provider>
     );
     expect(container).toMatchSnapshot();
+  });
+
+  it("should make todo complited when checkbox is clicked", () => {
+    render(
+        <Provider store={store}>
+        <DragDropContext>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {filteredTodos.map((todo, index) => (
+                  <Todo
+                    key={todo.id}
+                    id={todo.id}
+                    text={todo.text}
+                    index={index}
+                    isCompleted={todo.isCompleted}
+                  />
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </Provider>
+    );    
+
+    fireEvent.click(screen.getByRole('checkbox'));
+
+    expect(store.getActions()).toContainEqual(todoActions.compliteTodo("1"));
+  });
+
+  it("should delete when delete button is clicked", () => {
+    render(
+        <Provider store={store}>
+        <DragDropContext>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {filteredTodos.map((todo, index) => (
+                  <Todo
+                    key={todo.id}
+                    id={todo.id}
+                    text={todo.text}
+                    index={index}
+                    isCompleted={todo.isCompleted}
+                  />
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </Provider>
+    );    
+
+    fireEvent.click(screen.getByText('X'));
+
+    expect(store.getActions()).toContainEqual(todoActions.deleteTodo("1"));
   });
 });
