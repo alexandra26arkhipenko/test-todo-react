cc98958ba7110db9b1c02860c6872e0f83fd6801
aliaksandra.arkhipenka <aliaksandra.arkhipenka@godeltech.com>
2023-05-22 14:21:55 +0200

    tests are fixed

--- src/components/Todos/TodoFooter/TodoFilter/TodoFilter.test.js
+++ src/components/Todos/TodoFooter/TodoFilter/TodoFilter.test.js
@@ -4,6 +4,7 @@ import TodoFilter from "./TodoFilter";
 import { TODO_FOOTER_FILTER_ALL } from "../../../../constants/constants";
 import { render, fireEvent, screen } from "@testing-library/react";
 import configureMockStore from "redux-mock-store";
+import { todoActions } from "../../../../store/todo";
 
 const props = {
   onFilter: jest.fn(),
@@ -19,18 +20,5 @@ describe("TodoFilter", () => {
       </Provider>
     );
     expect(container).toMatchSnapshot();
-  });
-
-  it("calls clickHandler handler when button is clicked", () => {
-    // Arrange
-    const clickHandler = jest.fn();
-    render(<TodoFilter onFilter={clickHandler} />);
-
-    // Act
-    const allButton = screen.getByText(TODO_FOOTER_FILTER_ALL);
-    fireEvent.click(allButton);
-
-    // Assert
-    expect(clickHandler).toHaveBeenCalledWith(TODO_FOOTER_FILTER_ALL);
   });
 });
